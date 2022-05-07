337b83c57bcf9c86135e8e4e81f9c1bdfd9996ce
vanta blanta <vantablanta@gmail.com>
2022-05-07 22:20:12 +0300

    doc: migarte and psycopg added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from .main import main_blueprint
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
     app.register_blueprint(main_blueprint)
 
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    
     return app
 
 
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from enum import unique
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, unique= True)
+    email = db.Column(db.String, unique= True )
+    password = db.Column(db.Integer)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
