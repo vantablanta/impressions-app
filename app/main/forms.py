7fe6343b5149b24e4d2248869eefbe3a89efb323
vanta blanta <vantablanta@gmail.com>
2022-05-07 21:43:57 +0300

    ft: adds the register form

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField, PasswordField, EmailField, SubmitField
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
-    name = StringField('Enter name', validators=[DataRequired])
-    email = EmailField('Enter email', validators=[DataRequired])
-    password = PasswordField('Enter Password',validators=[DataRequired] )
-    password = PasswordField('Confirm Password',validators=[DataRequired] )
-    submit = SubmitField('Enter Password',validators=[DataRequired] )
+    name = StringField('Enter Name', validators=[DataRequired()])
+    email = EmailField('Enter Email', validators=[DataRequired()])
+    password = PasswordField('Enter Password',validators=[DataRequired()] )
+    confirm_password = PasswordField('Confirm Password',validators=[DataRequired()] )
+    submit = SubmitField('Submit')
 
