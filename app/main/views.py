dde152090eacf573d720ecf244542b1ef3321fff
vanta blanta <vantablanta@gmail.com>
2022-05-08 13:25:59 +0300

    pitches section added with upvotes and downvotes

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,52 @@
 from . import main_blueprint
 from flask import render_template
 from .forms import RegisterForm
-from flask_login import login_required
-from ...data import pitches
+# from flask_login import login_required
 
 
 @main_blueprint.route('/')
 def home():
-    form  = RegisterForm()
-    return render_template('index.html', form = form)
+    form = RegisterForm()
+    return render_template('index.html', form=form)
+
 
 @main_blueprint.route('/pitches')
 def pitches():
+    pitches = [
+        {
+            'poster': "Michelle",
+            "category": "Random",
+            "body": "Like it and leav it",
+            "upvotes": 8,
+            "downvotes": 0
+        },
+        {
+            'poster': "Avana",
+            "category": "Cars",
+            "body": "Like it and leav it",
+            "upvotes": 8,
+            "downvotes": 0
+        },
+        {
+            'poster': "Butile",
+            "category": "Crabs",
+            "body": "Like it and leav it",
+            "upvotes": 8,
+            "downvotes": 0
+        },
+        {
+            'poster': "Bankok",
+            "category": "Cake",
+            "body": "Like it and leav it",
+            "upvotes": 8,
+            "downvotes": 0
+        },
+        {
+            'poster': "Bamudo",
+            "category": "Health",
+            "body": "Like it and leav it",
+            "upvotes": 8,
+            "downvotes": 0
+        },
+    ]
     return render_template('pitches.html', pitches = pitches)
