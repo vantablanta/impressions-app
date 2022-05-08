4158b9870c0f472e8037ae6f1ddfb9cd52d96b96
vanta blanta <vantablanta@gmail.com>
2022-05-08 11:06:00 +0300

    bug: migaryion versions are created and db upgraded wiht our user model

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,9 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db = SQLAlchemy()
 
+db = SQLAlchemy()
+from .models import User
 
 def create_app(config_name):
     app = Flask(__name__)
