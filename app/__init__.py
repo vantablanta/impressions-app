6018dd0e78dd0d748d1eb0899c308bef941268ea
vanta blanta <vantablanta@gmail.com>
2022-05-08 16:23:44 +0300

    login url bug fixed

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ def create_app(config_name):
     from .main import main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint, url_prefix = '/auth')
  
     db.init_app(app)
     migrate = Migrate(app, db)
