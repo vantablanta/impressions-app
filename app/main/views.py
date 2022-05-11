112cd495e7c35fe047baae61a58b6b93a863791e
vanta blanta <vantablanta@gmail.com>
2022-05-11 12:31:06 +0300

    Heroku Deploy

--- app/main/views.py
+++ app/main/views.py
@@ -14,47 +14,6 @@ def home():
     return render_template('index.html')
 
 
-@main_blueprint.route('/pitches')
-def pitches():
-    pitches = [
-        {
-            "id": 1,
-            'poster': "Michelle",
-            "category": "Random",
-            "body": "Like it and leav it",
-            "comments": ["comment1", "comment2", "comment3", ],
-            "upvotes": 8,
-            "downvotes": 0
-        },
-    ]
-    return render_template('pitches.html', pitches=pitches)
-
-# @main_blueprint.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
-# @login_required
-# def comment(pitch_id):
-#     form = CommentForm()
-#     pitch = Pitch.query.get(pitch_id)
-#     all_comments = Comments.query.filter_by(pitch_id = pitch_id).all()
-#     if form.validate_on_submit():
-#         comment = form.comment.data 
-#         pitch_id = pitch_id
-#         user_id = current_user._get_current_object().id
-#         new_comment = Comments(comment = comment,user_id = user_id,pitch_id = pitch_id)
-#         db.session.add(new_comment)
-#         db.commit()
-#         return redirect(url_for('.comment', pitch_id = pitch_id))
-#     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
-
-@main_blueprint.route('/comment', methods=['GET', 'POST'])
-def comment():
-    form = CommentForm()
-    if form.validate_on_submit():
-        return redirect(url_for('main_blueprint.pitches'))
-    return render_template("new_comment.html", form = form )
-
-
-
-
 @main_blueprint.route('/user/<uname>')
 def profile(uname):
     user = User.query.filter_by(name=uname).first()
@@ -78,21 +37,50 @@ def update_profile(uname):
     return render_template('profile/update.html', form=form)
 
 
+@main_blueprint.route('/pitches')
+def pitches():  
+    pitches = Pitch.query.all()
+    return render_template('pitches.html', pitches=pitches)
+
 @main_blueprint.route('/pitch/add_new', methods=['GET', 'POST'])
 @login_required
 def add_pitch():
     form = PitchForm()
     if form.validate_on_submit():
-        # title = form.title.data
-        # category = form.category.data
-        # body = form.body.data
-        # user_id = current_user
-        # pitch = Pitch(title=title, category=category, body=body, user_id=current_user._get_current_object())
-        # db.session.add(pitch)
-        # db.session.commit()
+        title = form.title.data
+        category = form.category.data
+        body = form.body.data
+        user_id = current_user._get_current_object().id
+        pitch = Pitch(title=title, category=category, body=body, user_id=user_id)
+        db.session.add(pitch)
+        db.session.commit()
         return redirect(url_for('main_blueprint.home'))
     return render_template('new_pitch.html', form = form)
 
+
+
+# @main_blueprint.route('/comment/<int:pitch_id>', methods = ['POST','GET'])
+# @login_required
+# def comment(pitch_id):
+#     form = CommentForm()
+#     pitch = Pitch.query.get(pitch_id)
+#     all_comments = Comments.query.filter_by(pitch_id = pitch_id).all()
+#     if form.validate_on_submit():
+#         comment = form.comment.data 
+#         pitch_id = pitch_id
+#         user_id = current_user._get_current_object().id
+#         new_comment = Comments(comment = comment,user_id = user_id,pitch_id = pitch_id)
+#         db.session.add(new_comment)
+#         db.commit()
+#         return redirect(url_for('.comment', pitch_id = pitch_id))
+#     return render_template('comment.html', form =form, pitch = pitch,all_comments=all_comments)
+
+@main_blueprint.route('/comment', methods=['GET', 'POST'])
+def comment():
+    form = CommentForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main_blueprint.pitches'))
+    return render_template("new_comment.html", form = form )
 
 
 
--- app/models.py
+++ app/models.py
@@ -1,30 +1,33 @@
-from . import db,login_manager
+from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, current_user
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-    
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable = False)
-    email = db.Column(db.String(120), unique=True, nullable = False)
-    secure_password = db.Column(db.String(60),  nullable = False)
-    bio = db.Column(db.String(60),  default ="I love first impressions", nullable = False)
-    profile_picture = db.Column(db.String(300),  nullable = False, default='https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_960_720.png')
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    secure_password = db.Column(db.String(60),  nullable=False)
+    bio = db.Column(
+        db.String(60),  default="I love first impressions", nullable=False)
+    profile_picture = db.Column(db.String(
+        300),  nullable=False, default='https://cdn.pixabay.com/photo/2016/08/31/11/54/icon-1633249_960_720.png')
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
-    
-    def __init__(self,name, email, password) :
+
+    def __init__(self, name, email, password):
         self.name = name
         self.email = email
         self.password = password
 
     def __repr__(self):
         return f"User('{self.name}')"
-    
+
     @property
     def set_password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -34,16 +37,16 @@ class User(UserMixin, db.Model):
         self.secure_password = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self.secure_password,password)
+        return check_password_hash(self.secure_password, password)
+
 
 class Comments(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(120),nullable = False)
-    body = db.Column(db.String(120),nullable = False )
-    comments = db.Column(db.String(300),nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    
+    body = db.Column(db.String(120), nullable=False)
+    comments = db.Column(db.String(300), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
     def __repr__(self):
         return f"User('{self.category}', '{self.body}', '{self.comments}', '{self.upvotes}', '{self.downvotes}')"
 
@@ -55,27 +58,26 @@ class Comments(db.Model):
         self.body = body
 
     def save_comment(self):
-       Comments.all_comments.append(self)
+        Comments.all_comments.append(self)
 
-    @classmethod 
+    @classmethod
     def get_comments(cls):
         data = []
         for comment in cls.all_comments:
-           data.append(comment)
+            data.append(comment)
         return data
 
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(255),nullable = False)
-    category = db.Column(db.String(255), index = True,nullable = False)
-    body = db.Column(db.Text(), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), index=True, nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    
     def __init__(self, title, category, body, user_id):
-       self.title =  title
-       self.category  = category
-       self.body = body 
-       self. user_id = user_id
+        self.title = title
+        self.category = category
+        self.body = body
+        self. user_id = user_id
